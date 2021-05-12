ce1c5669343518c2611fcab41569702029f59c45
Kidus <kidzegeye@gmail.com>
2021-05-12 16:20:41 -0500

    pushing files to main

---
+++ app.py
@@ -0,0 +1,274 @@
+import json
+import os
+
+from db import DB
+from db import User, Student, Tutor, Subject, TutorSession, Messages, Invite
+from flask import Flask
+from flask import request
+
+app = Flask(__name__)
+DB_filename = "tutors.db"
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///%s" % DB_filename
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SQLALCHEMY_ECHO"] = True
+
+DB.init_app(app)
+with app.app_context():
+    DB.create_all()
+    if(Subject.query.filter_by(name="History").first() is None):
+        DB.session.add(Subject(name="History"))
+        DB.session.add(Subject(name="Math"))
+        DB.session.add(Subject(name="Computer Science"))
+        DB.session.add(Subject(name="Physics"))
+        DB.session.add(Subject(name="Chemistry"))
+        DB.session.commit()
+
+def get_user_by_email(email):
+    return User.query.filter(User.email == email).first()
+
+
+def get_user_by_session_token(session_token):
+    return User.query.filter(User.session_token == session_token).first()
+
+
+def get_user_by_update_token(update_token):
+    return User.query.filter(User.update_token == update_token).first()
+
+def missing_parameter_response(body, keys):
+    error_output="Missing parameter(s): "
+    for key in keys:
+        if body.get(key) is None:
+            error_output+="'{}', ".format(key)
+    return error_output[:-2]
+
+def success_response(data, code=200):
+    return json.dumps({"success":True, "data":data}), code
+
+def failure_response(message, code=404):
+    return json.dumps({"success":False, "error":message}), code
+
+
+@app.route("/api/users/")
+def get_users():
+    return success_response([u.serialize() for u in User.query.all()])
+
+@app.route("/api/students/")
+def get_students():
+    return success_response([s.serialize() for s in Student.query.all()])
+
+@app.route("/api/tutors/")
+def get_tutors():
+    return success_response([t.serialize() for t in Tutor.query.all()])
+
+@app.route("/api/user/<int:user_id>/")
+def get_user_by_id(user_id):
+    user = User.query.filter_by(id=user_id).first()
+    if user is None:
+        return failure_response("Given user id is not associated with a user!")
+    return success_response(user.serialize())
+
+"""
+@app.route("/api/users/", methods=["POST"])
+def create_user():
+    body = json.loads(request.data)
+    if body.get("netid") is None or body.get("name") is None or body.get("location") is None:
+        return failure_response(missing_parameter_response(body, ["netid", "name", "location"]))
+    n = body.get('name')
+    nid = body.get('netid')
+    loc = body.get('location')
+    new_user = User(name = n, netid = nid, location = loc)
+    DB.session.add(new_user)
+    DB.session.commit()
+    return success_response(new_user.serialize(), 201)
+"""
+
+@app.route("/api/user/<int:user_id>/tutor/", methods=["POST"])
+def add_tutor_to_user(user_id):
+    body = json.loads(request.data)
+    if body.get("description") is None:
+        return failure_response(missing_parameter_response(body, ["description"]))
+    user = User.query.filter_by(id=user_id).first()
+    if user is None:
+        return failure_response("Given user id is not associated with a user!")
+    desc = body.get("description")
+    rat = body.get("rating", 0)
+    new_tutor=Tutor(user_id = user_id, description = desc, rating= rat)
+    user.tutor.append(new_tutor)
+    DB.session.add(new_tutor)
+    DB.session.commit()
+    return success_response(new_tutor.serialize(),201)
+
+@app.route("/api/user/<int:user_id>/student/", methods=["POST"])
+def add_student_to_user(user_id):
+    body = json.loads(request.data)
+    if body.get("description") is None:
+        return failure_response(missing_parameter_response(body, ["description"]))
+    user = User.query.filter_by(id=user_id).first()
+    if user is None:
+        return failure_response("Given user id is not associated with a user!")
+    desc = body.get("description")
+    new_student=Student(user_id = user_id, description = desc)
+    user.student.append(new_student)
+    DB.session.add(new_student)
+    DB.session.commit()
+    return success_response(new_student.serialize(),201)
+
+@app.route("/api/user/<int:user_id>/subject/", methods=["POST"])
+def add_subject_to_user(user_id):
+    body = json.loads(request.data)
+    if body.get("name") is None: #or body.get("code") is None:
+        return failure_response(missing_parameter_response(body, ["name"]))#,"code"]))
+    user = User.query.filter_by(id=user_id).first()
+    if user is None:
+        return failure_response("Given user id is not associated with a user!")
+    name = body.get("name")
+    #code = body.get("code")
+    subject = Subject.query.filter_by(name=name).first()#,code=code).first()
+    if subject is None:
+        return failure_response("No subject found")
+    #Math, Science, English, History, Foreign Languages
+    if user.tutor is not None:
+        for t in user.tutor:
+            t.subjects.append(subject)
+    if user.student is not None:
+        for s in user.student:
+            s.subjects.append(subject)
+    DB.session.commit()
+    return success_response(subject.serialize())
+
+def get_subject_by_name(nme):
+    subject=Subject.query.filter_by(name=nme).first()
+    if(subject is None):
+        return None
+    return subject
+
+@app.route("/api/invite/", methods=["POST"])
+def create_invite():
+    body = json.loads(request.data)
+    if body.get("receiver_id") is None or body.get("subject_id") is None:
+        return failure_response(missing_parameter_response(body, ["receiver_id","subject_id"]))
+    receiver = User.query.filter_by(id=body.get("receiver_id")).first()
+    if receiver is None:
+        return failure_response("Receiver not found")
+
+    sender=get_logged_in_data()
+    if sender is None or type(sender)!=User:
+        return failure_response("Invalid session token")
+    if sender.id==receiver.id:
+        return failure_response("Can't send invite to yourself")
+    if receiver.tutor==[] or sender.student==[]:
+        return failure_response("Invalid user data")
+    tutor=None
+    student=None
+    for t in receiver.tutor:
+        tutor=t
+    for s in sender.student:
+        student=s
+    new_invite=Invite(receiver_id=tutor.id, sender_id=student.id, subject_id=body.get("subject_id"))
+    DB.session.add(new_invite)
+    DB.session.commit()
+    return success_response(new_invite.serialize())
+
+@app.route("/api/invite/<int:invite_id>/")
+def accept_invite(invite_id):
+    invite = Invite.query.filter_by(id=invite_id).first()
+    if invite is None:
+        return failure_response("Invite not found")
+    if invite.accepted:
+        return failure_response("Invite already accepted")
+    user=get_logged_in_data()
+    if user is None or type(user)!=User:
+        return failure_response("Invalid session token")
+    new_session = invite.create_session(user)
+    if new_session is None:
+        return failure_response("Authentification mismatch")
+    return success_response(new_session.rserialize())
+
+def extract_token(request):
+    auth_header = request.headers.get("Authorization")
+    if auth_header is None:
+        return False, json.dumps({"error":"Missing auth header"})
+
+    bearer_token = auth_header.replace("Bearer ","").strip()
+
+    if bearer_token is None or not bearer_token:
+        return False, json.dumps({"error":"Missing auth header"})
+
+    return True, bearer_token
+
+@app.route("/api/register/", methods=["POST"])
+def register_account():
+    body = json.loads(request.data)
+
+    if body.get("netid") is None or body.get("name") is None or body.get("location") is None or body.get("email") is None or body.get("password") is None:
+        return failure_response(missing_parameter_response(body, ["netid", "name", "location" ,"email", "password"]))
+    email = body.get("email")
+    password = body.get("password")
+    n = body.get('name')
+    nid = body.get('netid')
+    loc = body.get('location')
+
+
+    optional_user = get_user_by_email(email)
+
+    if optional_user is not None:
+        return json.dumps({"error": "User already exists"})
+
+    user = User(email=email, password=password,name = n, netid = nid, location = loc)
+
+    DB.session.add(user)
+    DB.session.commit()
+
+    return success_response(user.serialize(), 201)
+
+@app.route("/api/login/", methods=["POST"])
+def login():
+    body = json.loads(request.data)
+    email = body.get("email")
+    password = body.get("password")
+
+    if email is None or password is None:
+        return failure_response("Invalid email or password")
+
+    user = get_user_by_email(email)
+
+    success= user is not None and user.verify_password(password)
+    if not success:
+        return failure_response("Invalid email or password")
+    return success_response(user.serialize(), 201)
+
+
+@app.route("/api/session/", methods=["POST"])
+def update_session():
+    success, update_token = extract_token(request)
+
+    if not success:
+        return update_token
+
+    user = get_user_by_update_token(update_token)
+
+    if user is None:
+        return failure_response(f"Invalid update token: {update_token}")
+
+    user.renew_session()
+    DB.session.commit()
+
+    return success_response(user.serialize(), 201)
+
+
+
+def get_logged_in_data():
+    success, session_token = extract_token(request)
+
+    if not success:
+        return session_token
+
+    user = get_user_by_session_token(session_token)
+    if not user or not user.verify_session_token(session_token):
+        return None
+    return user
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
---
+++ db.py
@@ -0,0 +1,287 @@
+from flask_sqlalchemy import SQLAlchemy
+import datetime
+import hashlib
+import os
+import bcrypt
+
+DB = SQLAlchemy()
+
+def get_subject_by_name(nme):
+        subject=Subject.query.filter_by(name=nme).first()
+        if(subject is None):
+            return None
+        return subject
+#your classes here
+subjects_association = DB.Table(
+    'subjects_association',
+    DB.Column('subject_id', DB.Integer, DB.ForeignKey('subject.id')),
+    DB.Column('student_id', DB.Integer, DB.ForeignKey('student.id')),
+    DB.Column('tutor_id', DB.Integer, DB.ForeignKey('tutor.id'))
+)
+
+
+class User(DB.Model):
+    __tablename__ = "user"
+    id = DB.Column(DB.Integer, primary_key = True)
+    name = DB.Column(DB.String, nullable = False)
+    netid = DB.Column(DB.String, nullable = False)
+    location = DB.Column(DB.String, nullable = False)
+
+    email = DB.Column(DB.String, nullable=False, unique=True)
+    password_digest = DB.Column(DB.String, nullable=False)
+    session_token = DB.Column(DB.String, nullable=False, unique=True)
+    session_expiration = DB.Column(DB.DateTime, nullable=False)
+    update_token = DB.Column(DB.String, nullable=False, unique=True)
+
+    student = DB.relationship('Student', back_populates = "user", cascade='delete')
+    tutor = DB.relationship('Tutor', back_populates = "user", cascade='delete')
+    messages = DB.relationship("Messages", back_populates = "user")
+
+
+    def __init__(self, **kwargs):
+        self.name = kwargs.get('name')
+        self.netid = kwargs.get('netid')
+        self.location = kwargs.get('location')
+        self.email=kwargs.get("email")
+        self.password_digest = bcrypt.hashpw(kwargs.get("password").encode("utf8"), bcrypt.gensalt(rounds=13))
+        self.renew_session()
+
+    def serialize(self):
+        return {
+            "id":self.id,
+            "netid":self.netid,
+            "name":self.name,
+            "email":self.email,
+            "location":self.location,
+            "tutor": [t.rserialize() for t in self.tutor],
+            "student": [s.rserialize() for s in self.student],
+            "session_token":self.session_token,
+            "session_expiration": str(self.session_expiration),
+            "update_token": self.update_token,
+        }
+    def rserialize(self):
+        return {
+            "id":self.id,
+            "netid":self.netid,
+            "email":self.email,
+            "name":self.name,
+            "location":self.location
+        }
+
+    def _urlsafe_base_64(self):
+        return hashlib.sha1(os.urandom(64)).hexdigest()
+
+    def renew_session(self):
+        self.session_token = self._urlsafe_base_64()
+        self.session_expiration = datetime.datetime.now() + datetime.timedelta(days=1)
+        self.update_token = self._urlsafe_base_64()
+
+    def verify_password(self, password):
+        return bcrypt.checkpw(password.encode("utf8"), self.password_digest)
+
+    def verify_session_token(self, session_token):
+        return session_token == self.session_token and datetime.datetime.now() < self.session_expiration
+
+    def verify_update_token(self, update_token):
+        return update_token == self.update_token
+
+
+
+class Tutor(DB.Model):
+    __tablename__ = "tutor"
+    id = DB.Column(DB.Integer, primary_key = True)
+    user = DB.relationship('User', back_populates = "tutor", cascade='delete')
+    user_id= DB.Column(DB.Integer, DB.ForeignKey('user.id'),nullable=False)
+    subjects = DB.relationship('Subject', secondary = subjects_association, back_populates = 'tutor')
+    rating = DB.Column(DB.Integer, nullable= True)
+    description= DB.Column(DB.String,nullable=False)
+    sessions = DB.relationship("TutorSession", back_populates = "tutor", cascade='delete')
+    invites=DB.relationship("Invite",back_populates="receiver", cascade="delete")
+
+    def __init__(self, **kwargs):
+        self.user_id = kwargs.get('user_id')
+        self.description = kwargs.get('description')
+        self.rating=kwargs.get("rating")
+
+    def serialize(self):
+        return{
+            "id":self.id,
+            "rating":self.rating if (self.rating is not None) else 0,
+            "description":self.description,
+            "sessions":[a.rserialize() for a in self.sessions],
+            "subjects":[s.serialize() for s in self.subjects],
+            "user": self.user.rserialize(),
+            "invites":[i.serialize() for i in self.invites]
+        }
+    def rserialize(self):
+        return{
+            "id":self.id,
+            "rating":self.rating if (self.rating is not None) else 0,
+            "description":self.description,
+            "sessions":[a.rserialize() for a in self.sessions],
+            "subjects":[s.serialize() for s in self.subjects],
+        }
+
+class Student(DB.Model):
+    __tablename__ = "student"
+    id = DB.Column(DB.Integer, primary_key = True)
+    user = DB.relationship("User", back_populates = "student", cascade='delete')
+    user_id= DB.Column(DB.Integer, DB.ForeignKey('user.id'),nullable=False)
+    subjects = DB.relationship("Subject", secondary= subjects_association, back_populates="student")
+    description = DB.Column(DB.String, nullable=False)
+    sessions = DB.relationship("TutorSession",back_populates = "student", cascade='delete')
+    invites=DB.relationship("Invite",back_populates="sender", cascade="delete")
+
+    def __init__(self, **kwargs):
+        self.user_id = kwargs.get('user_id')
+        self.description = kwargs.get('description')
+
+    def serialize(self):
+        return {
+            "id":self.id,
+            "description":self.description,
+            "sessions":[a.rserialize() for a in self.sessions],
+            "subjects":[s.serialize() for s in self.subjects],
+            "user": self.user.rserialize(),
+            "invites_sent":[i.serialize() for i in self.invites]
+
+        }
+
+    def rserialize(self):
+        return {
+            "id":self.id,
+            "description":self.description,
+            "sessions":[a.rserialize() for a in self.sessions],
+            "subjects":[s.serialize() for s in self.subjects],
+        }
+
+class Subject(DB.Model):
+    __tablename__ = "subject"
+    id = DB.Column(DB.Integer, primary_key = True)
+    name = DB.Column(DB.String, nullable = False)
+    #code = DB.Column(DB.String, nullable = False)
+    student = DB.relationship('Student', secondary = subjects_association, back_populates = 'subjects')
+    tutor = DB.relationship('Tutor', secondary = subjects_association, back_populates = 'subjects')
+    sessions = DB.relationship('TutorSession', back_populates = "subject")
+    
+
+
+    def __init__(self, **kwargs):
+        #self.code = kwargs.get('code')
+        self.name = kwargs.get('name')
+
+    def serialize(self):
+        return {
+            "id":self.id,
+            #"code":self.code,
+            "name":self.name
+        }
+
+
+class TutorSession(DB.Model):
+    __tablename__ = "tutor_session"
+    id = DB.Column(DB.Integer, primary_key = True)
+    student = DB.relationship('Student', back_populates = "sessions")
+    student_id= DB.Column(DB.Integer, DB.ForeignKey('student.id'),nullable=False)
+    tutor = DB.relationship('Tutor', back_populates = "sessions")
+    tutor_id= DB.Column(DB.Integer, DB.ForeignKey('tutor.id'), nullable=False)
+    messages = DB.relationship('Messages', back_populates = "session", cascade = 'delete')
+    subject = DB.relationship('Subject', back_populates = "sessions")
+    subject_id= DB.Column(DB.Integer, DB.ForeignKey('subject.id'),nullable=False)
+    timestamp = DB.Column(DB.DateTime,default=datetime.datetime.now())
+
+    def __init__(self, **kwargs):
+        self.student_id = kwargs.get('student_id')
+        self.tutor_id = kwargs.get('tutor_id')
+        self.subject_id=kwargs.get("subject_id")
+
+    def serialize(self):
+        return {
+            "id":self.id,
+            "student":[self.student.serialize()],
+            "tutor":[self.tutor.serialize()],
+            "subject": [self.subject.serialize()],
+            "timestamp": self.timestamp.strftime("%D %T")
+        }
+
+    def rserialize(self):
+        return {
+            "id":self.id,
+            "student_id":self.student_id,
+            "tutor_id":self.tutor_id,
+            "subject": [self.subject.serialize()],
+            "timestamp": self.timestamp.strftime("%D %T")
+        }
+
+class Messages(DB.Model):
+    __tablename__ = "messages"
+    id = DB.Column(DB.Integer, primary_key = True)
+    message=DB.Column(DB.String, nullable=False)
+
+    session = DB.relationship("TutorSession", back_populates = "messages")
+    session_id= DB.Column(DB.Integer, DB.ForeignKey('tutor_session.id'),nullable=False)
+    user= DB.relationship("User", back_populates = "messages")
+    user_id= DB.Column(DB.Integer, DB.ForeignKey('user.id'),nullable=False)
+
+    def __init__(self, **kwargs):
+        self.message = kwargs.get('message')
+
+    def serialize(self):
+        return {
+            "id":self.id,
+            "sender":[self.user.serialze()],
+            "session":[self.session.serialize()],
+            "message":self.message
+        }
+
+class Invite(DB.Model):
+    __tablename__ = "invites"
+    id=DB.Column(DB.Integer, primary_key=True)
+    
+    sender_id=DB.Column(DB.Integer,DB.ForeignKey('student.id'),nullable=False)
+    sender=DB.relationship("Student", back_populates = "invites")
+
+    
+    receiver_id=DB.Column(DB.Integer,DB.ForeignKey('tutor.id'),nullable=False)
+    receiver=DB.relationship("Tutor", back_populates = "invites")
+
+    subject = DB.relationship('Subject')
+    subject_id= DB.Column(DB.Integer, DB.ForeignKey('subject.id'),nullable=False)
+    
+    accepted=DB.Column(DB.Boolean, default=False)
+
+    def __init__(self, **kwargs):
+        self.sender_id=kwargs.get("sender_id")
+        self.receiver_id=kwargs.get("receiver_id")
+        self.subject_id=kwargs.get("subject_id")
+
+    def serialize(self):
+        return {
+            "id":self.id,
+            "sender":[self.sender.rserialize()],
+            "receiver":[self.receiver.rserialize()],
+            "subject": [self.subject.serialize()],
+            "accepted": self.accepted
+        }
+
+    def rserialize(self):
+        return {
+            "id":self.id,
+            "sender_tid":self.sender_id.user_id,
+            "receiver":self.receiver_id.user_id,
+            "subject": self.subject.name,
+            "accepted": self.accepted
+        }
+    def create_session(self,receiver):
+        if receiver.email!=self.receiver.user.email:
+            return None
+        tutor = self.receiver
+        student = self.sender
+
+        new_session = TutorSession(student_id=student.id,tutor_id=tutor.id,subject_id=self.subject.id, accepted=True)
+        DB.session.add(new_session)
+        DB.session.commit()
+        return new_session
+
+
+    
